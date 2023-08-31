7322ac4781ec5f1279b34ef3a4e3c5c92dcfa56a
Jason Lee <huacnlee@gmail.com>
2023-08-31 16:55:09 +0800

    Fix index.md generate.

--- feishu-pages/src/index.ts
+++ feishu-pages/src/index.ts
@@ -30,36 +30,39 @@ const ROOT_NODE_TOKEN: string = process.env.ROOT_NODE_TOKEN || '';
   console.info('-------------------------------------------\n');
 
   const docs = await fetchAllDocs(feishuConfig.spaceId, 0, ROOT_NODE_TOKEN);
-  await fetchDocAndWriteFile(DOCS_DIR, '', docs);
+  await fetchDocAndWriteFile(DOCS_DIR, '', docs, 0);
 })();
 
 const fetchDocAndWriteFile = async (
   outputDir: string,
-  slugPrefix: string,
-  docs: Doc[]
+  parentSlug: string,
+  docs: Doc[],
+  depth: number = 0
 ) => {
   if (docs.length === 0) {
     return;
   }
-
-  fs.mkdirSync(outputDir, { recursive: true });
 
   for (let idx = 0; idx < docs.length; idx++) {
     const doc = docs[idx];
 
     let position = idx;
     let fileKey = normalizeSlug(doc.node_token);
-    let filename = path.join(outputDir, `${fileKey}.md`);
-    let fileSlug = path.join(slugPrefix, fileKey);
-    const folder = path.dirname(filename);
+    let fileSlug = path.join(parentSlug, fileKey);
+    let filename = path.join(outputDir, `${fileSlug}.md`);
 
-    // If is first child, named as index.md
-    if (idx === 0) {
-      filename = path.join(outputDir, `index.md`);
-      fileSlug = slugPrefix;
+    if (idx == 0) {
+      if (depth === 0) {
+        filename = path.join(outputDir, `index.md`);
+        fileSlug = '';
+      } else {
+        filename = path.join(outputDir, `${fileSlug}/index.md`);
+        fileSlug = fileSlug;
+      }
       position = -1;
     }
 
+    const folder = path.dirname(filename);
     fs.mkdirSync(folder, { recursive: true });
 
     const meta = generateFileMeta(doc, fileSlug, position);
@@ -75,8 +78,7 @@ const fetchDocAndWriteFile = async (
     console.info(' -> Writing doc', humanizeFileSize(content.length), '...');
     fs.writeFileSync(filename, out);
 
-    const subDir = path.join(outputDir, fileKey);
-    await fetchDocAndWriteFile(subDir, fileKey, doc.children);
+    await fetchDocAndWriteFile(outputDir, fileSlug, doc.children, depth + 1);
   }
 };
 
