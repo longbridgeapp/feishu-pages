87037dc863df9a7da3f484b1f48da6c779b368ed
ihavecoke <lb563@foxmail.com>
2023-11-01 19:37:02 +0800

    Fix the bug where nested 'children' were not being removed when removing nodes with 'meta' set to 'hide' in 'docs'.

--- feishu-pages/src/utils.ts
+++ feishu-pages/src/utils.ts
@@ -1,3 +1,5 @@
+import { FileDoc } from "./summary";
+
 export const normalizeSlug = (slug: string | number) => {
   // force convert slug into string
   slug = String(slug);
@@ -33,3 +35,46 @@ export const humanizeFileSize = (bytes, dp = 1) => {
 
   return bytes.toFixed(dp) + ' ' + units[u];
 };
+
+const allowKeys = [
+  "depth",
+  "title",
+  "slug",
+  "filename",
+  "node_token",
+  "parent_node_token",
+  "children",
+  "obj_create_time",
+  "obj_edit_time",
+  "obj_token",
+  "has_child",
+  "meta",
+  "position"
+];
+
+export function cleanupDocsForJSON(docs: FileDoc[]) {
+  const nodesToDelete = [];
+
+  for (let idx = 0; idx < docs.length; idx++) {
+    const doc = docs[idx];
+
+    Object.keys(doc).forEach(key => {
+      if (!allowKeys.includes(key)) {
+        delete doc[key];
+      }
+    });
+
+    if (doc.meta?.hide) {
+      nodesToDelete.push(idx);
+    }
+
+    if (doc.children) {
+      cleanupDocsForJSON(doc.children);
+    }
+  }
+
+  // Delete nodes in reverse order to avoid index issues
+  for (let i = nodesToDelete.length - 1; i >= 0; i--) {
+    docs.splice(nodesToDelete[i], 1);
+  }
+}
