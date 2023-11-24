5ac09f2ae68c04350a07d24b45caa07d7c1e91bc
Jason Lee <huacnlee@gmail.com>
2023-11-24 13:56:04 +0800

    Fix `ROOT_NODE_TOKEN` to support export itself.

--- feishu-pages/src/feishu.ts
+++ feishu-pages/src/feishu.ts
@@ -330,7 +330,7 @@ export const feishuFetchWithIterator = async (
   method: string,
   path: string,
   payload: Record<string, any> = {}
-): Promise<any[]> => {
+): Promise<WikiNode[]> => {
   let pageToken = '';
   let hasMore = true;
   let results: any[] = [];
@@ -362,6 +362,45 @@ export interface Doc {
   obj_token?: string;
   children: Doc[];
   has_child?: boolean;
+}
+
+/**
+ * 节点信息
+ *
+ * https://open.feishu.cn/document/server-docs/docs/wiki-v2/space-node/get_node
+ * https://open.feishu.cn/document/server-docs/docs/wiki-v2/space-node/list
+ */
+export interface WikiNode {
+  /**
+   * 知识空间 id
+   */
+  space_id: string;
+  /**
+   * 节点 token
+   */
+  node_token: string;
+  obj_token: string;
+  obj_type: 'doc' | 'docx' | 'sheet' | 'file';
+  /**
+   * 父节点 token。若当前节点为一级节点，父节点 token 为空
+   */
+  parent_node_token?: string;
+  /**
+   * 节点类型
+   *
+   * origin：实体
+   * shortcut：快捷方式
+   */
+  node_type: 'origin' | 'shortcut';
+  origin_node_token: string;
+  origin_space_id: string;
+  has_child: boolean;
+  title: string;
+  obj_create_time: string;
+  obj_edit_time: string;
+  node_create_time: string;
+  creator: string;
+  owner: string;
 }
 
 export { checkEnv, feishuConfig };
--- feishu-pages/src/wiki.ts
+++ feishu-pages/src/wiki.ts
@@ -1,7 +1,30 @@
-import { Doc, feishuFetchWithIterator } from './feishu';
+import { Doc, WikiNode, feishuFetch, feishuFetchWithIterator } from './feishu';
+
+/**
+ *
+ * @param spaceId https://open.feishu.cn/document/server-docs/docs/wiki-v2/space-node/get_node
+ * @param nodeToken
+ */
+export const fetchDocInfo = async (
+  spaceId: string,
+  nodeToken: string
+): Promise<WikiNode> => {
+  let data = await feishuFetch('GET', '/open-apis/wiki/v2/spaces/get_node', {
+    token: nodeToken,
+  });
+
+  const node = data.node as WikiNode;
+  if (!node) {
+    console.error('Node not found', nodeToken, data);
+  }
+
+  return node;
+};
 
 /**
  * 获取某个空间下的所有文档列表
+ *
+ * https://open.feishu.cn/document/server-docs/docs/wiki-v2/space-node/list
  *
  * @param {string} spaceId 空间 ID
  */
@@ -15,47 +38,63 @@ export const fetchAllDocs = async (
   }
 
   const prefix = '|__' + '___'.repeat(depth) + ' ';
-
-  let items = await feishuFetchWithIterator(
-    'GET',
-    `/open-apis/wiki/v2/spaces/${spaceId}/nodes`,
-    {
-      parent_node_token,
-      page_size: 50,
-    }
-  );
-
   let docs: Doc[] = [];
 
-  items
-    .filter((item) => item.obj_type == 'doc' || item.obj_type == 'docx')
-    .forEach((item) => {
-      const doc: Doc = {
-        depth: depth,
-        title: item.title,
-        node_token: item.node_token,
-        parent_node_token: parent_node_token,
-        obj_create_time: item.obj_create_time,
-        obj_edit_time: item.obj_edit_time,
-        obj_token: item.obj_token,
-        children: [],
-        has_child: item.has_child,
-      };
+  // Fetch fron root node
+  if (depth == 0 && parent_node_token) {
+    let rootNode = await fetchDocInfo(spaceId, parent_node_token);
+    let doc = {
+      depth: depth,
+      title: rootNode.title,
+      node_token: rootNode.node_token,
+      parent_node_token: null,
+      obj_create_time: rootNode.obj_create_time,
+      obj_edit_time: rootNode.obj_edit_time,
+      obj_token: rootNode.obj_token,
+      children: [],
+      has_child: rootNode.has_child,
+    };
+    docs.push(doc);
+  } else {
+    let items = await feishuFetchWithIterator(
+      'GET',
+      `/open-apis/wiki/v2/spaces/${spaceId}/nodes`,
+      {
+        parent_node_token,
+        page_size: 50,
+      }
+    );
 
-      docs.push(doc);
-    });
+    items
+      .filter((item) => item.obj_type == 'doc' || item.obj_type == 'docx')
+      .forEach((item) => {
+        const doc: Doc = {
+          depth: depth,
+          title: item.title,
+          node_token: item.node_token,
+          parent_node_token: parent_node_token,
+          obj_create_time: item.obj_create_time,
+          obj_edit_time: item.obj_edit_time,
+          obj_token: item.obj_token,
+          children: [],
+          has_child: item.has_child,
+        };
+
+        docs.push(doc);
+      });
+
+    console.info(
+      prefix + 'node:',
+      parent_node_token || 'root',
+      docs.length > 0 ? `${docs.length} docs` : ''
+    );
+  }
 
   // Ignore title `[hide]` or `[隐藏]`
   docs = docs.filter((doc) => {
     let title = doc.title.toLocaleLowerCase();
     return !title.includes('[hide]') && !title.includes('[隐藏]');
   });
-
-  console.info(
-    prefix + 'node:',
-    parent_node_token || 'root',
-    docs.length > 0 ? `${docs.length} docs` : ''
-  );
 
   for (const doc of docs) {
     if (doc.has_child) {
